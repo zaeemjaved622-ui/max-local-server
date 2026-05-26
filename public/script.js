f4dc11a45c3849f77d389bd67d7105f702a4398a
zaeemjaved622-ui <zaeemjaved622@gmail.com>
2026-05-26 18:37:33 +0500

    Add public/script.js

---
+++ public/script.js
@@ -0,0 +1,6 @@
+console.log("Max Local Server - شروع ہوگیا!");
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("سرور URL: http://localhost:8080");
+    console.log("اپنی PHP فائلیں public فولڈر میں رکھیں");
+});
